5977053bdca5449a500b5c6f5786fceec8dacb17
Erastus K. A. Arthur <erastusarthur@gmail.com>
2025-06-20 16:28:21 +0000

    age_calculator.py
    simple age finder based on inputs of birth year and the current year

---
+++ age_calculator.py
@@ -0,0 +1,8 @@
+print("WELCOME TO MY PAGE!!!")
+print("Let's find out how old you are")
+current_year = int(input("Enter the current year "))
+print(current_year)
+birth_year = int(input("Enter the birth year "))
+print(birth_year)
+age= current_year - birth_year
+print(f"Per your input, you are approximately {age} years old")
